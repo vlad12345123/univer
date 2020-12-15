c8fbfa882b4b374c884c5a28c9c17101f8f3d0ef
vlad12345123 <vzezjulin@gmail.com>
2020-12-15 04:21:34 +0300

    Update lab1.cs

--- lab1.cs
+++ lab1.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace lab6_1
 {
     class Program
     {
@@ -12,18 +12,32 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             A a = new A();
+            Console.WriteLine("Введите y или n\n");
+            a.query = Convert.ToChar(Console.ReadLine());
             Console.WriteLine("Значение поля c = " + a.c);
         }
     }
 
     class A
     {
-
         private float a = 10;
         private float b = 10;
+
+        public char query;
+
         public float c
         {
-            get { return a %= b; }
+            get
+            {
+                if (query == 'y')
+                {
+                    return a %= b;
+                }
+                else
+                {
+                    return a + b;
+                }
+            }
         }
 
         public A()
